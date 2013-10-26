e4da91fb5723c85f7cd47d3da9ed459e3743db7c
vinodkd <vinod@vinodkd.org>
2013-10-26 14:44:41 -0500

    updated webserver to handle puts and posts as well

--- webserver.js
+++ webserver.js
@@ -5,18 +5,22 @@ var http = require("http"),
     port = process.argv[2] || 8888;
 
 http.createServer(function(request, response) {
-
   var uri = url.parse(request.url).pathname
     , filename = path.join(process.cwd(), uri);
 
-  var contentTypesByExtension = {
-    '.html': "text/html",
-    '.css':  "text/css",
-    '.js':   "text/javascript"
-  };
+    switch(request.method){
+      case "GET" : handleGet(request,response,filename); break;
+      case "PUT" : handlePut(request,response,filename); break;
+      case "POST": handlePost(request,response,filename); break;
+    }
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+
+function handleGet (request,response,filename) {
 
   path.exists(filename, function(exists) {
-    if(!exists) {
+    if(!exists) { 
       response.writeHead(404, {"Content-Type": "text/plain"});
       response.write("404 Not Found\n");
       response.end();
@@ -26,12 +30,17 @@ http.createServer(function(request, response) {
     if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
     fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
+      if(err) {    // file cannot be read is an error for get; not put or post
         response.writeHead(500, {"Content-Type": "text/plain"});
         response.write(err + "\n");
         response.end();
         return;
       }
+      var contentTypesByExtension = {
+        '.html': "text/html",
+        '.css':  "text/css",
+        '.js':   "text/javascript"
+      };
 
       var headers = {};
       var contentType = contentTypesByExtension[path.extname(filename)];
@@ -41,6 +50,42 @@ http.createServer(function(request, response) {
       response.end();
     });
   });
-}).listen(parseInt(port, 10));
+}
 
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+function handlePost (request,response,filename) {
+  saveFile(request,response,filename);
+}
+
+function handlePut (request,response,filename) {
+  saveFile(request,response,filename);
+}
+
+function saveFile(request,response,filename) {
+  var dataToSave;
+  request.on('data', function(data) {
+    console.log(data + ">>>>");
+    dataToSave += data;
+    if(dataToSave.length > 1e6) {
+        dataToSave = "";
+        response.writeHead(413, {'Content-Type': 'text/plain'}).end();
+        request.connection.destroy();
+    }
+  });
+
+  request.on('end', function() {
+    fs.appendFile(filename,dataToSave,{"flag": "w"},function(err){
+      if(err){
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+      else{
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.end();
+        return;        
+      }
+    });
+  });
+
+}
